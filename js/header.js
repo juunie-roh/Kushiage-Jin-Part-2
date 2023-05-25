7bfbaac24c32c3271f83ad54178a58d3d52f415f
Juun <blacklily7513@gmail.com>
2023-05-25 13:03:32 +0900

    js fixed

--- js/header.js
+++ js/header.js
@@ -1,17 +1,31 @@
 const headerMenuBtn = document.getElementById('headerMenuBtn');
-const header = headerMenuBtn.parentElement.parentElement.parentElement;
-
-headerMenuBtn.addEventListener('click', function () {
-  headerMenuBtn.classList.toggle('active');
-  document.getElementById('lnb').classList.toggle('active');
-  header.classList.toggle('active');
-});
-
+const header = document.querySelector('header');
 const headerMenus = document.querySelectorAll('#lnb ol li a');
-for (menu of headerMenus) {
-  menu.addEventListener('click', function() {
-    headerMenuBtn.classList.remove('active');
-    document.getElementById('lnb').classList.remove('active');
-    header.classList.remove('active');
-  });
-}
+
+headerMenuBtn.addEventListener( 'click', () => {
+
+  headerMenuBtn.classList.toggle( 'active' );
+  document.getElementById('lnb').classList.toggle( 'active' );
+  header.classList.toggle( 'active' );
+
+} );
+
+headerMenus.forEach( (menu) => {
+
+  menu.addEventListener( 'click', () => {
+
+    headerMenuBtn.classList.remove( 'active' );
+    document.getElementById( 'lnb' ).classList.remove( 'active' );
+    header.classList.remove( 'active' );
+
+  } );
+
+} );
+
+// for (menu of headerMenus) {
+//   menu.addEventListener('click', function() {
+//     headerMenuBtn.classList.remove('active');
+//     document.getElementById('lnb').classList.remove('active');
+//     header.classList.remove('active');
+//   });
+// }
